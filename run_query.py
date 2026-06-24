ef06e994ed5039240ab3d3a85ae00a4d121f22bb
Blok <j.s.blok@cultureelerfgoed.nl>
2026-06-24 15:08:05 +0200

    cleaning

--- run_query.py
+++ run_query.py
@@ -1,5 +1,5 @@
 """ module to query two sparql endpoints, compare them, and write to file """
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta, datetime, timezone
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 # colored images for state indicator, because github markdown does not support color
@@ -11,11 +11,6 @@ STATE = {
 # nde datasetregister sparql endpoint
 nde_sparql = SPARQLWrapper('https://datasetregister.netwerkdigitaalerfgoed.nl/sparql')
 nde_sparql.setReturnFormat(JSON)
-
-# rce datacatalog sparql endpoint
-rce_sparql = SPARQLWrapper('https://api.linkeddata.cultureelerfgoed.nl/datasets/rce/datacatalog/services/datacatalog/sparql')
-rce_sparql.setReturnFormat(JSON)
-
 # query to count datasets on nde datasetregister published by rce and read within the last two days
 nde_sparql.setQuery(
     'PREFIX dct: <http://purl.org/dc/terms/>' \
@@ -28,12 +23,14 @@ nde_sparql.setQuery(
     '} ' 
 )
 
+# rce datacatalog sparql endpoint
+rce_sparql = SPARQLWrapper('https://api.linkeddata.cultureelerfgoed.nl/datasets/rce/datacatalog/services/datacatalog/sparql')
+rce_sparql.setReturnFormat(JSON)
 # query to count datasets in rce catalog
-rce_sparql.setQuery('''
-    PREFIX schema: <https://schema.org/>
-    SELECT (count(distinct ?dataset) as ?count) WHERE {
-    ?dataset schema:publisher <https://www.cultureelerfgoed.nl> .
-    } '''
+rce_sparql.setQuery(
+    'PREFIX schema: <https://schema.org/>' \
+    'SELECT (count(distinct ?dataset) as ?count) WHERE { ' \
+    '?dataset schema:publisher <https://www.cultureelerfgoed.nl> . }' \
 )
 
 try:
@@ -50,9 +47,10 @@ try:
         status = STATE['FAIL']
 
     # write to README
-    with open("README.md", "w") as f:
-        f.write(f'## Status <br /> \n')
-        f.write(f'{status} [{datetime.now():%Y-%m-%d %H:%M.%S}] {set_count_nde}/{set_count_rce} datasets uit de datacatalog van de RCE beschikbaar op het NDE Datasetregister.  <br /> \n')
+    with open('README.md', 'w', encoding='UTF8') as f:
+        f.write('## Status <br /> \n')
+        now_cet = (datetime.now(timezone.utc) + timedelta(hours=2)).replace(tzinfo=None)
+        f.write(f'{status} [{now_cet:%Y-%m-%d %H:%M}] <b>{set_count_nde}/{set_count_rce}</b> datasets uit de datacatalog van de RCE beschikbaar op het NDE Datasetregister.  <br /> \n')
         
-except Exception as e:
+except OSError as e:
     print(e)
