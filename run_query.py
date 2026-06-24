ddc5622459bd3a7fac419bfbfbbaa6e54610abcd
Blok <j.s.blok@cultureelerfgoed.nl>
2026-06-24 13:19:17 +0200

    commenting

--- run_query.py
+++ run_query.py
@@ -1,19 +1,22 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+""" module to query two sparql endpoints, compare them, and write to file """
 from datetime import date, timedelta, datetime
+from SPARQLWrapper import SPARQLWrapper, JSON
 
+# colored images for state indicator, because github markdown does not support color
 STATE = {
     'OK': '![#339900](https://placehold.co/5x5/339900/339900.png)',
-    'WARNING': '![#ec942c](https://placehold.co/5x5/ec942c/ec942c.png)',
     'FAIL': '![#f03c15](https://placehold.co/5x5/f03c15/f03c15.png)',
 }
 
+# nde datasetregister sparql endpoint
 nde_sparql = SPARQLWrapper('https://datasetregister.netwerkdigitaalerfgoed.nl/sparql')
 nde_sparql.setReturnFormat(JSON)
 
+# rce datacatalog sparql endpoint
 rce_sparql = SPARQLWrapper('https://api.linkeddata.cultureelerfgoed.nl/datasets/rce/datacatalog/services/datacatalog/sparql')
 rce_sparql.setReturnFormat(JSON)
 
-# query to count datasets on nde datasetregister published by rce and read recently
+# query to count datasets on nde datasetregister published by rce and read within the last two days
 nde_sparql.setQuery(
     'PREFIX dct: <http://purl.org/dc/terms/>' \
     'PREFIX schema: <https://schema.org/>' \
@@ -34,16 +37,19 @@ rce_sparql.setQuery('''
 )
 
 try:
+    # query and get results from json structure
     nde_q = nde_sparql.queryAndConvert()
     set_count_nde = nde_q['results']['bindings'][0]['count']['value']
     rce_q = rce_sparql.queryAndConvert()
     set_count_rce = rce_q['results']['bindings'][0]['count']['value']
 
+    # set color based on equal counts
     if set_count_nde == set_count_rce:
         status = STATE['OK']
     else:
         status = STATE['FAIL']
 
+    # write to README
     with open("README.md", "w") as f:
         f.write(f'## Status <br /> \n')
         f.write(f'{status} [{datetime.now():%Y-%m-%d %H:%M.%S}] {set_count_nde}/{set_count_rce} datasets uit de datacatalog van de RCE beschikbaar op het NDE Datasetregister.  <br /> \n')
